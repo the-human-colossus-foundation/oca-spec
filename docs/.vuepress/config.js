2a7055a6cdd84bc3927c8ba619bd518c7fabcaae
Robert Mitwicki <robert.mitwicki@humancolossus.org>
2025-02-06 09:04:13 +0100

    chore: Fix links and improve identation

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -70,7 +70,7 @@ export default defineUserConfig({
         text: "Specification",
         children: [
           {
-            text: "OCA v1.0.1",
+            text: "OCA v1.0.2",
             link: "/specification/README.md",
           },
           {
